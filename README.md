a5dceb51621797cf125a7dab6c58dc54462c6688
Pablo Gutiérrez <gutiii1997@gmail.com>
2022-01-17 18:08:14 +0100

    fix attributeType not being added

--- README.md
+++ README.md
@@ -7,3 +7,15 @@ Parser to convert GPX file into a valid GeoJson for Leaflet HeighGraph plugin.
 1. Install dependencies with `npm install` or `yarn`
 2. Move .gpx files into /input folder. They will be parsed and moved into /ouput folder with same file name.
 3. Run script using `npm run parse` or `yarn parse`
+
+## Config file
+
+You MUST create a `config.js` file into root folder with following structure:
+
+```js
+module.exports = {
+    creator: 'John Doe',
+    summary: 'Elevation',
+    removeWaypoints: true,
+}
+```
--- index.js
+++ index.js
@@ -1,31 +1,38 @@
 const { DOMParser } = require("xmldom")
 const fs = require("fs")
 const { gpx } = require("@mapbox/togeojson")
-const inputFolder = "./input"
-const outputFolder = "./output"
+const config = require("./config")
+const INPUT_FOLDER = "./input"
+const OUTPUT_FOLDER = "./output"
 
-const REMOVE_WAYPOINTS = true
-const PROPS_CREATOR = "José Carlos Gutiérrez"
-const PROPS_SUMMARY = "Altimetría"
+if (!fs.existsSync(INPUT_FOLDER)) fs.mkdirSync(INPUT_FOLDER)
+if (!fs.existsSync(OUTPUT_FOLDER)) fs.mkdirSync(OUTPUT_FOLDER)
 
-const inputFiles = fs.readdirSync(inputFolder, "utf-8")
-
+const inputFiles = fs.readdirSync(INPUT_FOLDER, "utf-8")
 inputFiles.forEach(parseGpx)
+
 function parseGpx(fileName) {
-    const inputFilePath = `${inputFolder}/${fileName}`
-    const outputFilePath = `${outputFolder}/${fileName}`.replace('.gpx', '.json')
+    const inputFilePath = `${INPUT_FOLDER}/${fileName}`
+    const outputFilePath = `${OUTPUT_FOLDER}/${fileName}`.replace('.gpx', '.json')
 
     const fileParsedFromDom = readFile(inputFilePath)
     const geoJsonFile = gpx(fileParsedFromDom)
+    
+    geoJsonFile.features = geoJsonFile.features.map(
+        (feature) => {
+            if (feature.properties?.attributeType === undefined) feature.properties.attributeType = '0'
+            return feature
+        }
+    )
 
-    if (REMOVE_WAYPOINTS) {
+    if (config.removeWaypoints) {
         geoJsonFile.features = geoJsonFile.features.filter(
             (feature) => feature.geometry.type !== "Point"
         )
     }
     geoJsonFile.properties = {}
-    if (PROPS_CREATOR) geoJsonFile.properties.creator = PROPS_CREATOR
-    if (PROPS_SUMMARY) geoJsonFile.properties.summary = PROPS_SUMMARY
+    if (config.creator) geoJsonFile.properties.creator = config.creator
+    if (config.summary) geoJsonFile.properties.summary = config.summary
 
     const outputText = JSON.stringify([geoJsonFile], null, 4)
     writeFile(outputFilePath, outputText)
